ca783f1b03a8ec73aaf9b0ed17d32df586ceb5ce
Sumit Mandal <sumitmandal749@gmail.com>
2021-05-03 19:36:21 +0530

    Added logo animation, firebase auth

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,22 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        logoLoader()
        
     }
     
-
+    func logoLoader() {
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        
+    }
+    
 }
